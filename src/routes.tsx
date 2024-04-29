a423604c0d16a8315772148b0a4c1671024ba456
David-AI-Expert <davidsasaki1986@gmail.com>
2024-04-29 11:20:42 -0700

    update the directory structure

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RepoPage from "./pages/RepoPage";
+import SettingPage from "./pages/SettingPage";
+
+const AppRoutes: React.FC = () => (
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/repo" element={<RepoPage />} />
+    <Route path="/setting" element={<SettingPage />} />
+  </Routes>
+);
+
+export default AppRoutes;
