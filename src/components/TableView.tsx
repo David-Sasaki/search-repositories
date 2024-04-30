7ae1ed1f83508d09ad61b6c6f018bb62b2e01ad4
David-AI-Expert <davidsasaki1986@gmail.com>
2024-04-30 13:11:11 -0700

    add a custom hook for API calls

--- src/components/TableView.tsx
+++ src/components/TableView.tsx
@@ -1,33 +1,18 @@
 import React, { useState, useEffect } from "react";
-import "../assets/styles/TableView.css";
-import fetchRepositories from "../services/api";
-import getFieldsFromJsonList from "../utils/jsonParser";
 import { useDebounce } from "../hooks/useDebounce";
-import Repository from "../utils/types";
+import { useFetchRepos } from "../hooks/useFetchRepos";
 import PaginationButtons from "./PaginationButtons";
+import "../assets/styles/TableView.css";
 
 const TableView: React.FC<{ query: string }> = ({ query }) => {
   const pageSize = Number(process.env.REACT_APP_PAGE_SIZE_FOR_TABLE_VIEW);
   const [columns, setColumns] = useState<string[]>([]);
   const [page, setPage] = useState<number>(0);
-  const [repositories, setRepositories] = useState<Repository[]>([]);
   const debouncedPage = useDebounce(
     page,
     Number(process.env.REACT_APP_DELAY_TIME)
   );
-
-  useEffect(() => {
-    if (query.length > 0) {
-      fetchRepositories(query, debouncedPage, pageSize)
-        .then((data) => {
-          setRepositories(getFieldsFromJsonList(data?.items || []));
-        })
-        .catch((error) => {
-          console.error("Error fetching repositories:", error);
-          setRepositories([]);
-        });
-    }
-  }, [debouncedPage, query]);
+  const repositories = useFetchRepos(query, debouncedPage, pageSize);
 
   const handlePageChange = (newPage: number) => {
     setPage(newPage);
---
+++ src/hooks/useFetchRepos.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import fetchRepositories from "../services/api";
+import getFieldsFromJsonList from "../utils/jsonParser";
+import Repository from "../utils/types";
+
+export function useFetchRepos(query: string, page: number, pageSize: number) {
+  const [repos, setRepos] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    if (query.length > 0) {
+      fetchRepositories(query, page, pageSize)
+        .then((data) => {
+          setRepos(getFieldsFromJsonList(data?.items || []));
+        })
+        .catch((error) => {
+          console.error("Error fetching repositories:", error);
+          setRepos([]);
+        });
+    }
+  }, [query, page, pageSize]);
+
+  return repos;
+}
